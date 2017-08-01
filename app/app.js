efa5e1d947bc1611f97101930e36237d596bef57
mistabenjo <benakehurst@hotmail.com>
2017-08-01 10:53:50 +0100

    Boilerplate for AngularJS Project
    Basic markup with app.js

---
+++ app/app.js
@@ -0,0 +1,14 @@
+(function(){
+    
+    'use strict';
+    
+    var appModule = angular.module("appModule", ["ngRoute"
+                                                ]);
+    
+    appModule.config(function(){
+        
+        
+        
+    });
+    
+})();
